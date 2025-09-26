1ae9c4d4dd6de2d8198d85afafe262be6edc2ef3
KayleeChan <kayleec55@nycstudents.net>
2025-09-26 12:18:48 -0400

    Added dogWalkShift method

--- src/DogWalker.java
+++ src/DogWalker.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+public class DogWalker
+{
+    /**
+     * Max # of dogs that a dog walker can walk simultaneously per hour
+     */
+    private int maxDogs;
+    /** company that the dog walker is associated with */
+    private DogWalkCompany company;
 
-public class DogWalker {
+    public DogWalker(int max, DogWalkCompany comp)
+    {
+        maxDogs = max;
+        company = comp;
+    }
+
+    public int walkDogs(int hour)
+    {
+        int dogs = company.numAvailableDogs(hour);
+        if (dogs < maxDogs)
+        {
+            company.updateDogs(hour, dogs);
+            return dogs;
+        }
+        else
+        {
+            company.updateDogs(hour, maxDogs);
+            return maxDogs;
+        }
+    }
+
+    public int dogWalkShift(int startHour, int endHour)
+    {
+        int pay = 0;
+        while(startHour <= endHour)
+        {
+            int dogs = walkDogs(startHour);
+            pay += dogs * 5;
+            if (dogs == maxDogs || startHour >= 9 && startHour <= 17)
+                pay += 3;
+            startHour++;
+        }
+        return pay;
+    }
 }
